86eae063ba1626241de1dffe1881ad2acfdc2d19
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-08-21 21:37:14 -0400

    added builder to start

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
-import { Context, Probot } from "probot";
+import { Probot } from "probot";
 
 export = (app: Probot) => {
   app.on("issues.opened", async (context) => {
     const issueComment = context.issue({
-      body: "You have opened an issue!\n Please make sure that your issue: \n - Is easily readible \n - Is related to the project \n Thank you! ~ *Alpro Bot* \n [My source code](https://github.com/TechPenguineer/Alpro-Bot)",
+      body: "Thanks for opening this issue!",
     });
     await context.octokit.issues.createComment(issueComment);
   });
+  // For more information on building apps:
+  // https://probot.github.io/docs/
 
-
-
+  // To get your app running against GitHub, see:
+  // https://probot.github.io/docs/development/
 };
+
