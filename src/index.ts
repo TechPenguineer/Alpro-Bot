4ee3771c15fc6084d2214df4ee2d4c88873751bb
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-08-21 21:47:16 -0400

    updated issue body

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Probot } from "probot";
 export = (app: Probot) => {
   app.on("issues.opened", async (context) => {
     const issueComment = context.issue({
-      body: "Thanks for opening this issue!\nA contributer will be with you soon.\n\n Thank you for using *Alpro-bot*!\nMade by: **[Tech Penguineer](https://github.com/TechPenguineer)**",
+      body: "# Thanks for opening this issue! \n A contributer will be with you soon.\n<hr>\n In the meantime you can check: \n - Does this issue makes sense \n - Is there a reason for this \n - Is this worth a contributors time \n - It is worded correctly \n Thank you for using *Alpro-bot*!\nMade by: **[Tech Penguineer](https://github.com/TechPenguineer)**",
       
       
     });
