51fdce3bf134b8ff20abd64ff17046c9815cf987
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-08-21 21:42:28 -0400

    updated new issue body

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ import { Probot } from "probot";
 export = (app: Probot) => {
   app.on("issues.opened", async (context) => {
     const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
+      body: "Thanks for opening this issue!\nA contributer will be with you soon.\n\n Thank you for using *Alpro-bot*!\nMade by: **[Tech Penguineer](https://github.com/TechPenguineer)**",
+      
+      
     });
     await context.octokit.issues.createComment(issueComment);
   });
