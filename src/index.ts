c3d37291716ce5b3d7a5a62fe14e4b05ff90924b
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-08-21 22:44:24 -0400

    fixed pull request webhook sender

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ export = (app: Probot) => {
     });
     await context.octokit.issues.createComment(issueComment);
   });
-  app.on("pull_request_review.submitted", async (context) => {
+  app.on("pull_request.opened", async (context) => {
     const issueComment = context.issue({
       body: "# Thanks for opening this Pull request! \nA contributer will be with you soon.\n<hr>\n In the meantime you can check: <br> - Does this PR makes sense <br> - Is there a reason for this <br> - Is this worth a contributors time <br> - It is worded correctly \n\n\n -- \n\n Thank you for using *Alpro-bot*!\nMade by: **[Tech Penguineer](https://github.com/TechPenguineer)**",
     });
