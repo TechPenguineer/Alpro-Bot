176cafb88d62ad9619eed460a5af438a1320e125
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-08-21 22:13:01 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,9 @@ import { Probot } from "probot";
 export = (app: Probot) => {
   app.on("issues.opened", async (context) => {
     const issueComment = context.issue({
-      body: "# Thanks for opening this issue! \n A contributer will be with you soon.\n<hr>\n In the meantime you can check: \n - Does this issue makes sense \n - Is there a reason for this \n - Is this worth a contributors time \n - It is worded correctly \n Thank you for using *Alpro-bot*!\nMade by: **[Tech Penguineer](https://github.com/TechPenguineer)**",
-      
-      
+      body: "# Thanks for opening this issue! \nA contributer will be with you soon.\n<hr>\n In the meantime you can check: <br> - Does this issue makes sense <br> - Is there a reason for this <br> - Is this worth a contributors time <br> - It is worded correctly \n\n\n -- \n\n Thank you for using *Alpro-bot*!\nMade by: **[Tech Penguineer](https://github.com/TechPenguineer)**",
     });
     await context.octokit.issues.createComment(issueComment);
   });
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 };
 
