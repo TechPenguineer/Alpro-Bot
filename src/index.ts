91069f7f08de20a75cdb72c357057c8abd5b4a80
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-08-21 21:27:24 -0400

    updated npm installs and updated new issue message body

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,13 @@
-import { Probot } from "probot";
+import { Context, Probot } from "probot";
 
 export = (app: Probot) => {
   app.on("issues.opened", async (context) => {
     const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
+      body: "You have opened an issue!\n Please make sure that your issue: \n - Is easily readible \n - Is related to the project \n Thank you! ~ *Alpro Bot* \n [My source code](https://github.com/TechPenguineer/Alpro-Bot)",
     });
     await context.octokit.issues.createComment(issueComment);
   });
-  // For more information on building apps:
-  // https://probot.github.io/docs/
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+
+
 };
